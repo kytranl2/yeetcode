34cf2ccb6e7ecd604cff0fb2af20e047a932049f
Ky <kygacc12@gmail.com>
2023-12-15 22:00:10 -0500

    added two boolean for left over pattern indication (nope)

--- yeetcode/isMatch.py
+++ yeetcode/isMatch.py
@@ -1,48 +1,58 @@
 def isMatch(s: str, p: str) -> bool:
-        def starRun(sl, c):
-            if c == '.' and len(sl) > 0:
-                while len(sl) > 0 and sl[0].isalpha():
-                    sl.pop(0)
-            elif len(sl) > 0:
-                while len(sl) > 0 and sl[0] == c:
-                    sl.pop(0)
-        sl = list(s)
-        sp = list(p)
-        index = 0
-        while index < len(sp):
-            if index < len(sp) - 1 and sp[index+1] == '*':
-                starRun(sl, sp[index])
-                index += 1
-            else:
-                if len(sl) > 0:
-                    if sp[index] != "." and sl[0] != sp[index]:
-                        break
-                    else:
-                        sl.pop(0)
+    hasP = False
+    hasRP = False
+    def starRun(sl, c):
+        if c == '.' and len(sl) > 0:
+            while len(sl) > 0 and sl[0].isalpha():
+                sl.pop(0)
+        elif len(sl) > 0:
+            while len(sl) > 0 and sl[0] == c:
+                sl.pop(0)
+    sl = list(s)
+    sp = list(p)
+    index = 0
+    while index < len(sp):
+        if index < len(sp) - 1 and sp[index+1] == '*':
+            starRun(sl, sp[index])
             index += 1
-
-        slr = list(s)
-        slr.reverse()
-        spr = list(p)
-        spr.reverse()
-        index = 0
-        while index < len(spr):
-            if index < len(spr) - 1 and spr[index] == '*':
-                starRun(slr, spr[index+1])
-                index += 1
-            else:
-                if len(slr) > 0:
-                    if spr[index] != "." and slr[0] != spr[index]:
-                        break
-                    else:
-                        slr.pop(0)
-            index+=1
-        if len(sl) == len(s) or len(slr) == len(s):
-            return False
-        elif len(sl) > 0 and len(slr) > 0:
-            return False
         else:
-            return True
+            if len(sl) > 0:
+                if sp[index] != "." and sl[0] != sp[index]:
+                    break
+                else:
+                    sl.pop(0)
+            else: ## more pattern
+                hasP = True
+
+                
+        index += 1
+
+    slr = list(s)
+    slr.reverse()
+    spr = list(p)
+    spr.reverse()
+    index = 0
+    while index < len(spr):
+        if index < len(spr) - 1 and spr[index] == '*':
+            starRun(slr, spr[index+1])
+            index += 1
+        else:
+            if len(slr) > 0:
+                if spr[index] != "." and slr[0] != spr[index]:
+                    break
+                else:
+                    slr.pop(0)
+            else:
+                hasRP = True
+        index+=1
+    if len(sl) == len(s) or len(slr) == len(s):
+        return False
+    elif len(sl) > 0 and len(slr) > 0:
+        return False
+    elif hasP and hasRP:
+        return False
+    else:
+        return True
 
 # print(isMatch("hhhhddd", "h*d*")) ## T
 # print(isMatch("hhhhddd", ".*")) ## T
@@ -56,10 +66,10 @@ def isMatch(s: str, p: str) -> bool:
 # print(isMatch("aaa", "a*a")) ## T
 # print(isMatch("aaa", "a*aa")) ## T
 # print(isMatch("aaca", "ab*a*c*a")) ## T
-# print(isMatch("aaa", "ab*a*c*a")) ## T
+print(isMatch("aaa", "ab*a*c*a")) ## T
 # print(isMatch("bbbba", ".*a*a")) ## T
 # print(isMatch("ab", ".*..")) ## T
 # print(isMatch("mississippi", "mis*is*ip*."))## T
 # print(isMatch("abbbcd", "ab*bbbcd"))##T
 # print(isMatch("abcdede", "ab.*de"))##T
-print(isMatch("aaa", "aaaa"))##T
+# print(isMatch("aaa", "aaaa"))##F
