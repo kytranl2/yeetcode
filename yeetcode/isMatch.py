ab0524792f21d36e3e5ee9961fa5df9733a9d740
Ky Tran <kygacc12@gmail.com>
2023-12-18 21:37:23 -0500

    added counter (nope)

--- yeetcode/isMatch.py
+++ yeetcode/isMatch.py
@@ -1,16 +1,37 @@
 def isMatch(s: str, p: str) -> bool:
+    ptotal = 0
+    sl = list(s)
+    sp = list(p)
+    stotal = 0
+
+    for c in s:
+        stotal += 1
+    
+    for index in range(len(sp)):
+        if sp[index] == '*' or (index < len(sp) - 1 and sp[index+1] == '*'):
+            continue
+        else:
+            ptotal +=1
+
     hasP = False
-    hasRP = False
     def starRun(sl, c):
+        nonlocal stotal
+        nonlocal ptotal
         ## remove the amount of non star char minus same char in s 
         if c == '.' and len(sl) > 0:
             while len(sl) > 0 and sl[0].isalpha():
-                sl.pop(0)
+                if stotal > ptotal:
+                    sl.pop(0)
+                    stotal -= 1
+                else:
+                    break
         elif len(sl) > 0:
             while len(sl) > 0 and sl[0] == c:
-                sl.pop(0)
-    sl = list(s)
-    sp = list(p)
+                if stotal > ptotal:
+                    sl.pop(0)
+                    stotal -= 1
+                else:
+                    break
     index = 0
     while index < len(sp):
         if index < len(sp) - 1 and sp[index+1] == '*':
@@ -22,35 +43,19 @@ def isMatch(s: str, p: str) -> bool:
                     break
                 else:
                     sl.pop(0)
+                    stotal -= 1
+                    ptotal -= 1
             else: ## more pattern
                 hasP = True
 
                 
         index += 1
 
-    slr = list(s)
-    slr.reverse()
-    spr = list(p)
-    spr.reverse()
-    index = 0
-    while index < len(spr):
-        if index < len(spr) - 1 and spr[index] == '*':
-            starRun(slr, spr[index+1])
-            index += 1
-        else:
-            if len(slr) > 0:
-                if spr[index] != "." and slr[0] != spr[index]:
-                    break
-                else:
-                    slr.pop(0)
-            else:
-                hasRP = True
-        index+=1
-    if len(sl) == len(s) or len(slr) == len(s):
+    if len(sl) == len(s):
         return False
-    elif len(sl) > 0 and len(slr) > 0:
+    elif len(sl) > 0:
         return False
-    elif hasP and hasRP:
+    elif hasP:
         return False
     else:
         return True
@@ -67,10 +72,11 @@ def isMatch(s: str, p: str) -> bool:
 # print(isMatch("aaa", "a*a")) ## T
 # print(isMatch("aaa", "a*aa")) ## T
 # print(isMatch("aaca", "ab*a*c*a")) ## T
-print(isMatch("aaa", "ab*a*c*a")) ## T
+# print(isMatch("aaa", "ab*a*c*a")) ## T
 # print(isMatch("bbbba", ".*a*a")) ## T
 # print(isMatch("ab", ".*..")) ## T
 # print(isMatch("mississippi", "mis*is*ip*."))## T
 # print(isMatch("abbbcd", "ab*bbbcd"))##T
 # print(isMatch("abcdede", "ab.*de"))##T
 # print(isMatch("aaa", "aaaa"))##F
+print(isMatch("aabcbcbcaccbcaabc", ".*a*aa*.*b*.c*.*a*"))##T
